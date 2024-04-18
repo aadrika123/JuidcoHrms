ecc42dbbc7fdd7daab3b2d82c647dcb5f77780f9
krish vishwakarma <kkrish7654@gmail.com>
2024-04-18 10:33:24 +0530

    payslip

--- backend/src/component/juidcoHrms/dao/employee/payslip.dao.ts
+++ backend/src/component/juidcoHrms/dao/employee/payslip.dao.ts
@@ -5,8 +5,56 @@ import { generateRes } from "../../../../util/generateRes";
 const prisma = new PrismaClient();
 
 class PayslipDao {
+  // get = async (req: Request) => {
+  //   const emp_id = req.query.emp_id;
+  //   const query: Prisma.employeesFindFirstArgs = {
+  //     select: {
+  //       emp_salary_details: {
+  //         select: {
+  //           emp_salary_allow: true,
+  //           emp_salary_deduction: true,
+  //         },
+  //       },
+
+  //       emp_join_details: {
+  //         select: {
+  //           acc_number: true,
+  //           designation: true,
+  //           pay_scale: true,
+  //         },
+  //       },
+  //       emp_basic_details: {
+  //         select: {
+  //           emp_name: true,
+  //         },
+  //       },
+  //     },
+  //     where: {
+  //       emp_id: String(emp_id),
+  //     },
+  //   };
+
+  //   const data: any = await prisma.employees.findFirst(query);
+  //   const count_allow_and_deduction = await prisma.$queryRaw<any[]>`
+  //           SELECT
+  //           SUM(emp_allow.amount_in) as total_allowance , SUM(emp_deduct.amount_in) as total_deductions
+  //           FROM
+  //               employees as emp
+  //           JOIN
+  //               employee_salary_details as sal_details ON emp.emp_salary_details_id = sal_details.id
+  //           JOIN
+  //               employee_salary_allow as emp_allow ON sal_details.id = emp_allow.employee_salary_details_id
+  //           JOIN
+  //               employee_salary_deduction as emp_deduct ON sal_details.id = emp_deduct.employee_salary_details_id
+  //            WHERE emp_id=${emp_id}
+  //       `;
+  //   data.total = { ...count_allow_and_deduction[0] };
+  //   return generateRes(data);
+  // };
+
   get = async (req: Request) => {
-    const emp_id = req.query.emp_id;
+    const { emp_id, date } = req.query;
+
     const query: Prisma.employeesFindFirstArgs = {
       select: {
         emp_salary_details: {
@@ -31,10 +79,14 @@ class PayslipDao {
       },
       where: {
         emp_id: String(emp_id),
+        // created_at: String(date),
       },
     };
 
     const data: any = await prisma.employees.findFirst(query);
+    const payroll = await prisma.$queryRaw`
+      SELECT * FROM payroll_master WHERE emp_id=${emp_id} AND date=Date(${date});
+    `;
     const count_allow_and_deduction = await prisma.$queryRaw<any[]>`
             SELECT 
             SUM(emp_allow.amount_in) as total_allowance , SUM(emp_deduct.amount_in) as total_deductions
@@ -49,6 +101,7 @@ class PayslipDao {
              WHERE emp_id=${emp_id}
         `;
     data.total = { ...count_allow_and_deduction[0] };
+    data.payroll = payroll;
     return generateRes(data);
   };
 }
