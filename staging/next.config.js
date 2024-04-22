b329a81dfcd0fa28d80c86ab160283ec677a43e7
krish vishwakarma <kkrish7654@gmail.com>
2024-04-22 16:50:14 +0530

    resbasing code

--- staging/next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/hrms",
-  env: {
-    backend: "https://aadrikainfomedia.com/auth",
-  },
-};
-
-module.exports = nextConfig;
