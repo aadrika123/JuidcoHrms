b637ad29baa821ed7161bb51b9763dfab199ff8a
Krish Vishwakarma <64971157+Kkrish7654@users.noreply.github.com>
2024-04-22 12:26:37 +0530

    Update axiosConfig.ts

--- frontend/src/lib/axiosConfig.ts
+++ frontend/src/lib/axiosConfig.ts
@@ -7,8 +7,8 @@ axios.defaults.headers.common["Authorization"] = setHeader();
 const instance = axios.create({
   // baseURL: "http://teamwork.ddnsfree.com:3001/api/v1/finance",
 
-  baseURL: "http://127.0.0.1:8000/api/hrms/v1/", // --> need to change in 7001
-
+ // baseURL: "http://127.0.0.1:8000/api/hrms/v1/", // --> need to change in 7001
+  baseURL: `${process.env.backend}/api/hrms/v1`,
   // baseURL: `${process.env.backend}/api/hrms/v1`,
   // You can add other default configurations here
 });
