6ee3261be2501c1c8080ca9464e04983c61077e0
krish vishwakarma <kkrish7654@gmail.com>
2024-05-14 16:24:05 +0530

    migrate

---
+++ backend/prisma/migrations/20240514080450_ddo_optional/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "employee_office_details" ALTER COLUMN "office_name" DROP NOT NULL,
+ALTER COLUMN "office_code" DROP NOT NULL,
+ALTER COLUMN "ddo_designation" DROP NOT NULL,
+ALTER COLUMN "ddo_code" DROP NOT NULL;
