d632582f8e9cde42f9d6c647082bc57354dc4ec9
krish vishwakarma <kkrish7654@gmail.com>
2024-05-15 14:33:08 +0530

    all

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,10 @@ installModules () {
 
 configure(){
     rm  ./backend/prisma/seeder/foreignWrapper.seed.ts
-    cp ./staging/foreignWrapper.seed.ts ./backend/prisma/seeder/
+    cp ./deploy/foreignWrapper.seed.ts ./backend/prisma/seeder/
 
     rm ./frontend/next.config.js
-    cp ./staging/next.config.js ./frontend
+    cp ./deploy/next.config.js ./frontend
 }
 
 resetDatabases() {
@@ -28,7 +28,8 @@ resetDatabases() {
     echo "TWILIO_ACCNT_SID=\"AC9568828d649d47f5865843700bbf0a8c\"" >> .env
     echo "TWILIO_AUTH_TOKEN=\"8c288639cacd7f5162fc7c99a7ad2a74\"" >> .env
     echo "TWILIO_PHONE=\"+16562269475\"">> .env
-    npx prisma migrate dev --name init
+    npx prisma migrate dev
+    npx prisma migrate deploy
     cd ..
 }
 
