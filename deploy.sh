224821e48194d1c7d820bd2691039492d126886d
krish vishwakarma <kkrish7654@gmail.com>
2024-05-27 21:14:39 +0530

    stage

--- deploy.sh
+++ deploy.sh
@@ -28,7 +28,7 @@ resetDatabases() {
     echo "TWILIO_ACCNT_SID=\"AC9568828d649d47f5865843700bbf0a8c\"" >> .env
     echo "TWILIO_AUTH_TOKEN=\"8c288639cacd7f5162fc7c99a7ad2a74\"" >> .env
     echo "TWILIO_PHONE=\"+16562269475\"">> .env
-    npx prisma migrate dev
+    # npx prisma migrate dev
     npx prisma migrate deploy
     cd ..
 }
