2af9bda5dc951b025db2ed8ade72b349db93d78f
Kaushal <kaushalkantmishra127@gmail.com>
2024-11-05 17:00:56 +0530

    added grade pay section

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,8 +12,8 @@
 const nextConfig = {
   basePath: "/hrms",
   env: {
-    backend: "https://aadrikainfomedia.com/auth",
-    // backend: "http://localhost:8000",
+    // backend: "https://aadrikainfomedia.com/auth",
+    backend: "http://localhost:8000",
   },
   // async rewrites() {
   //   return [
