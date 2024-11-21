bdd4ef217fd4c64e45f110dcc6218a7d803a7951
kaushalkantmishra <kaushalkantmishra127@gmail.com>
2024-11-21 16:37:40 +0530

    fixed payroll and payslip

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -12,8 +12,8 @@
 const nextConfig = {
   basePath: "/hrms",
   env: {
-    // backend: "https://aadrikainfomedia.com/auth",
-    backend: "http://localhost:7001",
+    backend: "https://aadrikainfomedia.com/auth",
+    // backend: "http://localhost:7001",
   },
   images: {
     domains: ["aadrikainfomedia.com", "jharkhandegovernance.com"], // Replace with your image's domain
