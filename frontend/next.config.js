9c8ceabb9d28efb6e9526e9883dfb5d431e9b60d
kaushal <kaushalkantmishra127@gmail.com>
2025-07-02 12:58:23 +0530

    modification in next.config

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,10 +4,10 @@ const nextConfig = {
   env: {
 
     // backend: "https://aadrikainfomedia.com/auth",
-    // backend: "https://jharkhandegovernance.com/auth",
+    backend: "https://jharkhandegovernance.com/auth",
     // backend: "http://localhost:7001",
     // backend: "https://egov.rsccl.in/auth",
-    backend: "http://localhost:8000",
+    // backend: "http://localhost:8000",
   },
   images: {
     domains: ["aadrikainfomedia.com", "jharkhandegovernance.com"],
